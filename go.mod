0fce10b79afd4ed4a93fab51b6d6aece4674cd8d
xaaaaaanny <nazar.tovstenko02@gmail.com>
2025-05-22 15:45:58 +0300

    server init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xaaaaaanny/Chirpy
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
